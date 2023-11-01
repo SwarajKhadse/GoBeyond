2ba37cc8320032715e594b170c59b0aad34f3fb0
SwarajKhadse <khadseswaraj@gmail.com>
2023-11-01 11:54:57 -0700

    initial commit\

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+}
